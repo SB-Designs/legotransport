dd2e8cc31f51f153af75baae7bf2a8debe51d97a
SB Designs <hello@trackle.serv00.net>
2024-12-07 13:57:54 +0000

    Update and rename script.js to llr.js

--- llr.js
+++ llr.js
@@ -1,5 +1,5 @@
 function playSound(soundNumber) {
     // Get the audio file corresponding to the button clicked
-    const audio = new Audio(`sounds/sound${soundNumber}.mp3`);
+    const audio = new Audio(`llr-sounds/sound${soundNumber}.mp3`);
     audio.play();
 }
